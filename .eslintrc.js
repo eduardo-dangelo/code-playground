be29ea8cae23782eeb9f005dc2520bec458ccfb3
eduardodangelo <eduardo@siteassist.co>
2022-10-26 20:33:04 +0100

    modify eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,11 +26,7 @@ module.exports = {
     'indent': ['error', 2],
     'react/prop-types': 0,
     'object-curly-spacing': ['error', 'always'],
-    'no-unused-vars': 'error', // or "@typescript-eslint/no-unused-vars": "off",
-    'unused-imports/no-unused-vars': 'error',
-    'react/jsx-uses-react': 0,
-    'react/js-uses-react': 0,
-    'react/jsx-uses-vars': 0,
-    'react/js-uses-vars': 0,
+    'no-unused-vars': 'off', // or "@typescript-eslint/no-unused-vars": "off",
+    'unused-imports/no-unused-vars': 'off',
   }
 }
