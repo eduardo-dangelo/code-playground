d6cdc41b649c376bdf1388133147eb9c12b48f28
eduardodangelo <eduardo@siteassist.co>
2022-11-01 22:18:44 +0000

    created sidebar using material

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-import { TodoList} from "./components/TodoList";
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+import { TodoList } from './components/TodoList'
+import Sidebar from './components/Sidebar'
 
 function App() {
   return (
     <div className="App">
-      <TodoList/>
+      <Sidebar />
+      <TodoList />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
