ed14c855987c0b361be9c7ee03e13ab100c8fdc1
eduardodangelo <eduardo@siteassist.co>
2022-11-08 21:33:36 +0000

    set up router

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,8 +1,15 @@
 import { Box } from '@mui/system'
 import { List, ListItem, ListItemButton, ListItemText, Divider } from '@mui/material'
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
-const Sidebar = ({ items }) => {
+const Sidebar = ({ items = [] }) => {
+  const navigate = useNavigate()
+
+  const handleItemClick = (route) => {
+    navigate(route)
+  }
+
   return (
     <Box
       sx={{
@@ -10,18 +17,18 @@ const Sidebar = ({ items }) => {
         maxWidth: 360,
         bgcolor: '#2196f3',
         color: 'white',
-        height: '100vh'
+        height: '100vh',
       }}
-    > 
+    >
       <nav aria-label="main mailbox folders">
         <List>
           <ListItem>
-            <ListItemText primary='CODE PLAYGROUND' />
-          </ListItem> 
-          <Divider/>
+            <ListItemText primary="CODE PLAYGROUND" />
+          </ListItem>
+          <Divider />
           {items.map((item, index) => {
             return (
-              <ListItem disablePadding key={index}>
+              <ListItem disablePadding key={index} onClick={() => handleItemClick(item.route)}>
                 <ListItemButton>
                   <ListItemText primary={item.name} />
                 </ListItemButton>
