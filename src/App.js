824eb301b1d640ba474af1fd79b28349fce5f9cb
eduardodangelo <eduardo@siteassist.co>
2022-11-08 21:34:24 +0000

    add router

--- src/App.js
+++ src/App.js
@@ -4,22 +4,27 @@ import './App.css'
 import { TodoList } from './components/TodoList'
 import Sidebar from './components/Sidebar'
 import { Box } from '@mui/material'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 const siteData = [
   { name: 'Todo List', route: '/todo-list' },
-  { name: 'Basic Form', route: '/basic-form', component: TodoList }
+  { name: 'Basic Form', route: '/basic-form' },
 ]
 
 function App() {
   return (
-    <Box
-      sx={{
-        display: 'flex',
-      }}
-    >
-      <Sidebar items={siteData} />
-      <TodoList />
-    </Box>
+    <Router>
+      <Box
+        sx={{
+          display: 'flex',
+        }}
+      >
+        <Sidebar items={siteData} />
+        <Routes>
+          <Route path="/todo-list" element={<TodoList />} />
+        </Routes>
+      </Box>
+    </Router>
   )
 }
 
